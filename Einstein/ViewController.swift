431275a33db77d0643753f9ce4a7c01f8fed5d12
Adam Wulf <adam.wulf@gmail.com>
2021-04-13 23:46:26 -0500

    Initial Commit

---
+++ Einstein/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Einstein
+//
+//  Created by Adam Wulf on 4/13/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
