82eee7bf60069e2c3606229a09663e110b6f705a
Adam Wulf <adam.wulf@gmail.com>
2021-08-10 08:36:57 +0200

    Adding a view controller to show environment variables up through the currently selected line. Also adding a toolbar to eventually toggle the panes.

---
+++ Einstein/EnvironmentViewController.swift
@@ -0,0 +1,14 @@
+//
+//  EnvironmentViewController.swift
+//  Einstein
+//
+//  Created by Adam Wulf on 8/10/21.
+//  Copyright © 2021 Milestone Made. All rights reserved.
+//
+
+import Foundation
+import SwiftTex
+import IosMath
+
+class EnvironmentViewController: NSViewController {
+}
